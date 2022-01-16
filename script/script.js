d70c9db9aafe9cf4b5bd203ef7136fa89f72c39f
Olexandr Klein <olexandrkleyn@gmail,com>
2022-01-16 15:29:19 +0300

    final

---
+++ script/script.js
@@ -0,0 +1,9 @@
+$('.main-carousel').flickity({
+    // options
+    cellAlign: 'center',
+    wrapAround: true,
+    selectedAttraction: 0.01,
+    friction: 0.15,
+    resize: true,
+    groupCells: 1
+});
